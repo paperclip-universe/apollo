61e9469285f989ad71fe9a1cc9ca6c7ec9e5cc5e
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-01-03 17:42:13 -0500

    well

--- apollo-libretro-cores/apollo-libretro-beetle-vb/build.rs
+++ apollo-libretro-cores/apollo-libretro-beetle-vb/build.rs
@@ -2,5 +2,5 @@ use apollo_libretro_build::build;
 
 fn main() {
     #[cfg(not(target_os = "macos"))]
-    build("beetle-vb-libretro", Some("mednafen_vb.patch"));
+    build("beetle-vb-libretro", Some("mednafen_vb.patch"), None);
 }
