a3b85ee136e392303e8c684ed3097e03ea811c0f
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-01-04 17:49:59 -0500

    temp ignore on windows

--- apollo-libretro-cores/apollo-libretro-parallel-64/src/lib.rs
+++ apollo-libretro-cores/apollo-libretro-parallel-64/src/lib.rs
@@ -1,3 +1,6 @@
 pub fn get_path() -> Option<&'static str> {
-    Some(env!("CPATH"))
+    #[cfg(not(target_os = "windows"))]
+    return Some(env!("CPATH"));
+    #[cfg(target_os = "macos")]
+    return None;
 }
