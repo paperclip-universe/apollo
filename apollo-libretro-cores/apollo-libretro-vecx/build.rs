649579e59fafd02f542d0fa9132da773b46589a4
Wyatt-Stanke <wyattstanke@icloud.com>
2023-01-05 17:28:10 +0000

    really

--- apollo-libretro-cores/apollo-libretro-vecx/build.rs
+++ apollo-libretro-cores/apollo-libretro-vecx/build.rs
@@ -1,5 +1,9 @@
 use apollo_libretro_build::build;
 
 fn main() {
-    build("libretro-vecx", Some("vecx.patch"), Some("Makefile.libretro"));
+    build(
+        "libretro-vecx",
+        Some("vecx.patch"),
+        Some("Makefile.libretro"),
+    );
 }
