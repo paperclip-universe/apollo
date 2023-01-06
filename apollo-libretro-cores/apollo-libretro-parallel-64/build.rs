991c4c498e8f8e9783b7cd292a09074af9fc25d0
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-01-06 08:26:18 -0500

    quandale dingle

--- apollo-libretro-cores/apollo-libretro-parallel-64/build.rs
+++ apollo-libretro-cores/apollo-libretro-parallel-64/build.rs
@@ -1,6 +1,6 @@
 use apollo_libretro_build::build;
 
 fn main() {
-    #[cfg(any(target_os = "windows", target_os = "macos"))]
+    #[cfg(not(any(target_os = "windows", target_os = "macos")))]
     build("parallel-n64", Some("parallel_n64.patch"), None);
 }
