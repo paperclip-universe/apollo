6241d4cc71ff794e21c410a74e270351ce2a473f
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-01-07 11:33:27 -0500

    Remove all conditional compilation

--- apollo-libretro-cores/apollo-libretro-vecx/build.rs
+++ apollo-libretro-cores/apollo-libretro-vecx/build.rs
@@ -1,7 +1,6 @@
 use apollo_libretro_build::build;
 
 fn main() {
-    // #[cfg(not(target_os = "macos"))]
     build(
         "libretro-vecx",
         Some("vecx.patch"),
--- apollo-libretro-cores/apollo-libretro-vecx/src/lib.rs
+++ apollo-libretro-cores/apollo-libretro-vecx/src/lib.rs
@@ -1,6 +1,3 @@
 pub fn get_path() -> Option<&'static str> {
-    #[cfg(not(target_os = "macos"))]
     return Some(env!("CPATH"));
-    #[cfg(target_os = "macos")]
-    return None;
 }
