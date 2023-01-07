4780d84c7736cded214517e38b0ea4496921ad41
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-01-07 11:40:54 -0500

    Format

--- apollo-libretro/apollo-libretro-cores/src/lib.rs
+++ apollo-libretro/apollo-libretro-cores/src/lib.rs
@@ -7,21 +7,21 @@ use apollo_libretro_freechaf::get_path as freechaf_get_path;
 use apollo_libretro_freeintv::get_path as freeintv_get_path;
 use apollo_libretro_gearcoleco::get_path as gearcoleco_get_path;
 use apollo_libretro_genesis_plus_gx::get_path as genesis_plus_gx_get_path;
+use apollo_libretro_handy::get_path as handy_get_path;
+use apollo_libretro_melonds::get_path as melonds_get_path;
 use apollo_libretro_mgba::get_path as mgba_get_path;
-use apollo_libretro_handy::get_path as handy_get_path; 
-use apollo_libretro_melonds::get_path as melonds_get_path; 
-use apollo_libretro_mupen64plus_next::get_path as mupen64plus_next_get_path; 
-use apollo_libretro_neocd::get_path as neocd_get_path; 
-use apollo_libretro_nestopia_ue::get_path as nestopia_ue_get_path; 
-use apollo_libretro_o2em::get_path as o2em_get_path; 
-use apollo_libretro_opera::get_path as opera_get_path; 
-use apollo_libretro_parallel_64::get_path as parallel_64_get_path; 
-use apollo_libretro_prosystem::get_path as prosystem_get_path; 
-use apollo_libretro_snes9x::get_path as snes9x_get_path; 
-use apollo_libretro_stella_2014::get_path as stella_2014_get_path; 
-use apollo_libretro_vecx::get_path as vecx_get_path; 
-use apollo_libretro_virtual_jaguar::get_path as virtual_jaguar_get_path; 
-use apollo_libretro_yabause::get_path as yabause_get_path; 
+use apollo_libretro_mupen64plus_next::get_path as mupen64plus_next_get_path;
+use apollo_libretro_neocd::get_path as neocd_get_path;
+use apollo_libretro_nestopia_ue::get_path as nestopia_ue_get_path;
+use apollo_libretro_o2em::get_path as o2em_get_path;
+use apollo_libretro_opera::get_path as opera_get_path;
+use apollo_libretro_parallel_64::get_path as parallel_64_get_path;
+use apollo_libretro_prosystem::get_path as prosystem_get_path;
+use apollo_libretro_snes9x::get_path as snes9x_get_path;
+use apollo_libretro_stella_2014::get_path as stella_2014_get_path;
+use apollo_libretro_vecx::get_path as vecx_get_path;
+use apollo_libretro_virtual_jaguar::get_path as virtual_jaguar_get_path;
+use apollo_libretro_yabause::get_path as yabause_get_path;
 
 pub fn get_all() -> Vec<&'static str> {
     const FUNCTIONS: [fn() -> Option<&'static str>; 24] = [
@@ -48,7 +48,7 @@ pub fn get_all() -> Vec<&'static str> {
         stella_2014_get_path,
         vecx_get_path,
         virtual_jaguar_get_path,
-        yabause_get_path
+        yabause_get_path,
     ];
 
     FUNCTIONS.into_iter().filter_map(|x| x()).collect()
