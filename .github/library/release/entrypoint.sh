6291188c08859d5fa01bbf300226cd70a760bfaf
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-02-02 17:06:40 -0500

    foob

--- .github/library/release/entrypoint.sh
+++ .github/library/release/entrypoint.sh
@@ -3,7 +3,7 @@
 apt-get -y update
 apt-get -y install \
     curl git \
-    nasm make gcc
+    nasm make gcc libssl-dev pkg-config
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly -y
 . "$HOME/.cargo/env"
