1e9d910c43b68b18e691028bf809b46e9258ab7d
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2023-02-05 15:28:22 -0500

    fix formatting

--- apollo-hyper/apollo-hyper-libretro/apollo-hyper-libretro-build/src/dir.rs
+++ apollo-hyper/apollo-hyper-libretro/apollo-hyper-libretro-build/src/dir.rs
@@ -1,4 +1,7 @@
-use std::{env::var, path::{Path, PathBuf}};
+use std::{
+    env::var,
+    path::{Path, PathBuf},
+};
 
 pub fn get_out_dir() -> PathBuf {
     Path::new(&var("OUT_DIR").unwrap()).join(var("CARGO_PKG_NAME").unwrap())
